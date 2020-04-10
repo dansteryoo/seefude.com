7dc63a3db6f348d3b0bdfbe25a5921e7cf093e44
dansteryoo <dansteryoo@gmail.com>
2020-04-10 14:21:25 -0400

    scss index.js

--- config/keys_dev.js
+++
@@ -1,7 +0,0 @@
-// module.exports = {
-//     fsClientId20: 'b9f4d796964048b293a62e8b2dfc280d',
-//     fsClientSecret20: 'b07fded7209a45248033bbc647856f6d', 
-//     fsCustomerId10: 'b9f4d796964048b293a62e8b2dfc280d',
-//     fsCustomerSecret10: 'f4af5240c191429386c18da8cfd382c5',
-//     fsAPI: 'http://platform.fatsecret.com/rest/server.api'
-// }
