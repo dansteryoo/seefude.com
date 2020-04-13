adb5955f3544acd8579685d4d7269caa55fe19da
dansteryoo <dansteryoo@gmail.com>
2020-04-13 15:00:59 -0400

    transitions

---
+++ src/styles/nutrition_facts.js
@@ -0,0 +1,29 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+
+// span
+
+/* #total-calories
+
+
+#total-calories
+#total-fat
+#total-saturated-fat
+#total-cholesterol
+#total-sodium
+#total-potassium
+#total-carbohydrate-fiber
+#total-dietary-fiber
+#total-sugar
+#total-protein
+#total-calcium
+#total-iron
+#total-vitamin-C
+#total-vitamin-A
+
+*/
+
+
+
+})
